be5034546b50faa10435b58820b56a5c5b2113f7
Ben Beaumont <Benhab1@aol.com>
2024-09-05 18:56:36 +0100

    Created a function to play the game

--- index.js
+++ index.js
@@ -27,3 +27,46 @@ function getHumanChoice(){
   }
 };
 
+function playRound(){
+  const computerChoice = getComputerChoice();
+  const humanChoice = getHumanChoice();
+
+  let result;
+
+  if(humanChoice === 'rock' && computerChoice === 'rock'){
+    result = 'tied';
+    score.Ties++;
+  } else if(humanChoice === 'rock' && computerChoice === 'paper'){
+    result = 'lost';
+    score.computerWins++;
+  } else if(humanChoice === 'rock' && computerChoice === 'scissors'){
+    result = 'win';
+    score.playerWins++;
+  } else if(humanChoice === 'paper' && computerChoice === 'rock'){
+    result = 'win';
+    score.playerWins++;
+  } else if(humanChoice === 'paper' && computerChoice === 'paper'){
+    result = 'tied';
+    score.Ties++;
+  } else if(humanChoice === 'paper' && computerChoice === 'scissors'){
+    result = 'lost';
+    score.computerWins++;
+  } else if(humanChoice === 'scissors' && computerChoice === 'rock'){
+    result = 'lost';
+    score.computerWins++;
+  } else if(humanChoice === 'scissors' && computerChoice === 'paper'){
+    result = 'win';
+    score.playerWins++;
+  } else if(humanChoice === 'scissors' && computerChoice === 'scissors'){
+    result = 'tied';
+    score.Ties++;
+  }
+  console.log(`You picked ${humanChoice} the computer picked ${computerChoice} the result is, you ${result}`);
+  console.log(`The score is player score: ${score.playerWins} computer score: ${score.computerWins} and ties: ${score.Ties}`);
+};
+
+playRound();
+
+
+
+
